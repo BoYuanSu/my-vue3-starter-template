c0c1281460e70375d6c79542a568085de148bec0
Paul Su <paul.su@ohbottech.com>
2021-08-09 09:32:26 +0800

    config vite legacy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,18 @@ export default defineConfig({
     vue(),
     visualizer({
       open: true
+    }),
+    legacy({
+      targets: [
+        'ie >= 11',
+        'Android > 39',
+        'Chrome >= 60',
+        'Safari >= 10.1',
+        'iOS >= 10.3',
+        'Firefox >= 54',
+        'Edge >= 15'
+      ],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
     })
   ]
 })
